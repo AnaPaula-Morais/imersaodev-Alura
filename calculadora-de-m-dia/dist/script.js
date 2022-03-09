ac083b008bb71d67a604c005edc726858da8fd7f
Ana Paula <ap.almeidapm@gmail.com>
2022-03-09 23:30:41 +0000

    Uma nova versão da calculadora de média melhorada e uma troca de nome das pastas

--- calculadora-de-m-dia/dist/script.js
+++ calculadora-de-m-dia/dist/script.js
@@ -1,27 +1,36 @@
-var nome = "Maria"
-
-var notaDoPrimeiroBimestre = 9
-var notaDoSegundoBimestre = 7
-var notaDoTerceiroBimestre = 4
-var notaDoQuartoBimestre = 2
-
-var soma = notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre
-var notaFinal = soma / 4
-var notaFixada = notaFinal.toFixed (1)
-var texto = "Bem vinda! " + nome + " sua média foi " + notaFixada
-//console.log(texto)
-if (notaFixada > 7.0) {
-  texto = texto + " Parabens você foi aprovada!!"
-} else {
-  texto = texto + " Infelizmente foi reprovada, estude mais!"
+function Registrar() {
+  var notaDoPrimeiroBimestre = parseFloat(document.getElementById("valor1").value);
+ 
+  var notaDoSegundoBimestre = parseFloat(document.getElementById("valor2").value);
+  
+  var notaDoTerceiroBimestre = parseFloat(document.getElementById("valor3").value);
+  
+  var notaDoQuartoBimestre = parseFloat(document.getElementById("valor4").value);
+  
+  var soma = notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre
+  var valorFinal = soma / 4
+  var notaFixada = valorFinal.toFixed (1)
+  console.log(notaFixada)
+ 
+  var texto = "Bem vindo(a)! Sua média foi " + notaFixada
+  console.log(texto)
+ 
+  if (notaFixada > 7.0) {
+  texto = texto + " Parabens você foi aprovado(a)!!"
+} 
+else {
+  texto = texto + " Infelizmente foi reprovado(a), estude mais!"
 }
-console.log(texto)
+  console.log(texto)
+  
+  var p = document.createElement("p");
+  p.textContent = texto;
+  p.className="page-subtitle"
+  document.getElementById("container").appendChild(p);
 
-var p = document.createElement("p");
-p.textContent = texto;
-p.className="page-subtitle"
-document.getElementById("container").appendChild(p);
 
-
+}
+  
+  
 //revisão
 //variáveis, string, console.log, toFixed,operações matemáticas, concatenação
